ff7e15647c2923a7144d1ef2afba29f894777939
assane savadogo <assane.savadogo81@gmail.com>
2019-07-26 18:08:49 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_all?(collection)
-
+  block_return_values = []
+  i = 0 
+  while i < collection.size 
+  block_return_values << yield(collection[i])
+  i += 1 
+  binding.pry
+  end
 end
