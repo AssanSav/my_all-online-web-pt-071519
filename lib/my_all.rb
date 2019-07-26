eca2d0904c574a551c58fe1e524886f1e2123aff
assane savadogo <assane.savadogo81@gmail.com>
2019-07-26 18:10:44 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
-
+  block_return_values = []
+  i = 0 
+  while i < collection.size 
+  block_return_values << yield(collection[i])
+  i += 1 
+  #binding.pry
+   end
+  if block_return_values.include?(false)
+    false 
+  else 
+    true 
+  end
 end
